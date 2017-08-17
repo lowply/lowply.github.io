412a2a3efe1714356c23fd32b3207fc3491aa8b9
Sho Mizutani <lowply@github.com>
2017-08-17 21:49:52 +0900

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 hljs.initHighlightingOnLoad();
 
 $(function(){
-	var path = "fixture.jp" + $(".hb-button.http a").attr("href")
+	var path = "fixture.jp" + $(".hb-button.old a").attr("href")
 	var hb_url = "http://b.hatena.ne.jp/entry/" + path
 	var hb_count_img = "https://b.hatena.ne.jp/entry/image/http://" + path
 	$(".hb-button.old small a").attr("href", hb_url)
