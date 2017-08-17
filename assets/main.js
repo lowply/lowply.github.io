19d7d0e3ee3abb98e772b65738b11bd197ea335e
Sho Mizutani <lowply@github.com>
2017-08-17 21:45:11 +0900

    Change hb button name

--- assets/main.js
+++ assets/main.js
@@ -4,6 +4,6 @@ $(function(){
 	var path = "fixture.jp" + $(".hb-button.http a").attr("href")
 	var hb_url = "http://b.hatena.ne.jp/entry/" + path
 	var hb_count_img = "https://b.hatena.ne.jp/entry/image/http://" + path
-	$(".hb-button.http small a").attr("href", hb_url)
-	$(".hb-button.http small a").append('<img src="' + hb_count_img + '" />')
+	$(".hb-button.old small a").attr("href", hb_url)
+	$(".hb-button.old small a").append('<img src="' + hb_count_img + '" />')
 })
